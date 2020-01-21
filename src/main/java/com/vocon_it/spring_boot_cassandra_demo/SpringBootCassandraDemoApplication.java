40be7fbade5dbbdb161f29f84c362ee8de5d6a05
Oliver Veits <oliver.veits@gmail.com>
2020-01-21 15:58:00 +0100

    first commit

---
+++ src/main/java/com/vocon_it/spring_boot_cassandra_demo/SpringBootCassandraDemoApplication.java
@@ -0,0 +1,13 @@
+package com.vocon_it.spring_boot_cassandra_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCassandraDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCassandraDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vocon_it/spring_boot_cassandra_demo/SpringBootCassandraDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vocon_it.spring_boot_cassandra_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCassandraDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

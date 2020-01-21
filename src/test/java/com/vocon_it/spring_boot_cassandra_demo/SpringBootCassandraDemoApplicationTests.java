8a088ffcb69d67b6c789cc184b3e8e57bb008853
Oliver Veits <oliver.veits@arbeitsagentur.de>
2020-01-21 19:11:05 +0100

    commented out the changes since cassandra-unit does not seem to work...

--- src/test/java/com/vocon_it/spring_boot_cassandra_demo/SpringBootCassandraDemoApplicationTests.java
+++ src/test/java/com/vocon_it/spring_boot_cassandra_demo/SpringBootCassandraDemoApplicationTests.java
@@ -1,13 +1,56 @@
 package com.vocon_it.spring_boot_cassandra_demo;
 
+//import com.datastax.driver.core.Cluster;
+//import com.datastax.driver.core.Session;
+//import org.cassandraunit.CQLDataLoader;
+//import org.cassandraunit.dataset.cql.ClassPathCQLDataSet;
+//import org.cassandraunit.spring.CassandraDataSet;
+//import org.cassandraunit.spring.CassandraUnitTestExecutionListener;
+//import org.cassandraunit.spring.EmbeddedCassandra;
+//import org.cassandraunit.spring.CassandraUnit;
+//import org.cassandraunit.utils.EmbeddedCassandraServerHelper;
+//import org.junit.BeforeClass;
 import org.junit.jupiter.api.Test;
+//import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.TestExecutionListeners;
+//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+//
+//import static org.springframework.test.context.TestExecutionListeners.MergeMode.MERGE_WITH_DEFAULTS;
+
 
 @SpringBootTest
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@TestExecutionListeners(
+//		listeners = CassandraUnitTestExecutionListener.class,
+//		mergeMode = MERGE_WITH_DEFAULTS
+//)
+//@CassandraUnit
+//@CassandraDataSet(value = { "simple.cql" })
+//@EmbeddedCassandra( configuration = "cassandra-embedded-config.yaml" )
 class SpringBootCassandraDemoApplicationTests {
+
+//	@BeforeClass
+//	public static void before() throws Exception
+//	{
+//		EmbeddedCassandraServerHelper.startEmbeddedCassandra(20000L);
+//		Cluster cluster = new Cluster.Builder().addContactPoints("127.0.0.1").withPort(9042).build();
+//		Session session = cluster.connect();
+//		CQLDataLoader cqlDataLoader = new CQLDataLoader(session);
+//		cqlDataLoader.load(new ClassPathCQLDataSet("cql/schema.sql", true, true, "keyspace"));
+//	}
 
 	@Test
 	void contextLoads() {
 	}
+
+//	@Test
+//	public void should_have_started_and_execute_cql_script() throws Exception {
+//		CqlSession session = EmbeddedCassandraServerHelper.getSession();
+//
+//		ResultSet result = session.execute("select * from mytable WHERE id='myKey01'");
+//		assertThat(result.iterator().next().getString("value"), is("myValue01"));
+//	}
+
 
 }
